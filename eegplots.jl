b3c6b5a626b79dd05f9589572face91d20ed2f8a
mjalves98 <mjalves98@gmail.com>
2017-06-08 10:56:41 -0400

    formatting, power in decibels

--- eegplots.jl
+++ eegplots.jl
@@ -1,18 +1,23 @@
 using PyPlot, DSP
 
 #displays the spectrogram of a chosen channel of a Spectrogram object
-function plot_spectrogram(axes, spect::Spectrogram, channel_num::Int64, plot_title::String="")
+function plot_spectrogram(axes, spect::Spectrogram, channel_num::Int64,
+  plot_title::String="")
   p = spect.power_all[channel_num]
+  println(minimum(p))
+  #TODO instead of extent using t, use first & last time_bins and convert to sec
   t = spect.time
   f = spect.freq_bins
   fs = spect.analog_data.fs
-  axes[:imshow](flipdim(log10(p),1), extent=[first(t), last(t),
-          fs*first(f), fs*last(f)], aspect="auto")
+  img = axes[:imshow](flipdim(p,1), extent=[first(t), last(t),
+  fs*first(f), fs*last(f)], aspect="auto")
+  colorbar(img)
   title(plot_title)
 end
 
-#plot a channel of an analog data object 
-function plot_time(axes, eeg_data::AnalogData, channel_num::Int64, plot_title::String="")
+#plot a channel of an analog data object
+function plot_time(axes, eeg_data::AnalogData, channel_num::Int64,
+  plot_title::String="")
   xall = eeg_data.x_all
   x = xall[channel_num, :]
   ti = eeg_data.t
--- eegtypes.jl
+++ eegtypes.jl
@@ -1,38 +1,46 @@
 using OpenEphysLoader, DSP, PyPlot
 
 type AnalogData
-    x_all::Array{Float64,2} #data, with each row being a different channel and each column a time value
-    t::Vector{Float64} #time
-    original_fs::Int64
-    fs::Int64
-    channel_nums::Vector{Int64} #desired channel numbers
-  end
-
-#create analogdata object with only data and time- assumes all channels are desired and that fs is 30,000
-function AnalogData(x_all::Array{Float64,2}, t::Vector{Float64}; original_fs::Int64=30000, channel_nums::Vector{Int64}=[0,0])
-    if channel_nums == nothing
-      channel_nums = collect(1:size(x_all)[1])
-    end
-    fs = original_fs
-    AD = AnalogData(x_all::Array{Float64,2}, t::Vector{Float64}, original_fs::Int64, fs::Int64, channel_nums::Vector{Int64})
+  #data, with each row being a different channel and each column a time value
+  x_all::Array{Float64,2}
+  t::Vector{Float64} #time
+  original_fs::Int64
+  fs::Int64
+  channel_nums::Vector{Int64} #desired channel numbers
 end
 
-#given a file path and sampling rate, returns an array of the data and an array of the time values
+#create analogdata object with only data and time- assumes all channels are
+#desired and that fs is 30,000
+function AnalogData(x_all::Array{Float64,2}, t::Vector{Float64};
+  original_fs::Int64=30000, channel_nums::Vector{Int64}=[0,0])
+  if channel_nums == nothing
+    channel_nums = collect(1:size(x_all)[1])
+  end
+  fs = original_fs
+  AD = AnalogData(x_all::Array{Float64,2}, t::Vector{Float64},
+  original_fs::Int64, fs::Int64, channel_nums::Vector{Int64})
+end
+
+#given a file path and sampling rate, returns an array of the data and an
+#array of the time values
 function load_continuous(path::String, fs::Int64)
   A = nothing
   open("$path", "r") do io
     A = Array(SampleArray(Float64, io))
-      #sleep(0.1)
-      seekstart(io)
-      T = Array(TimeArray(Float64, io))
-      return(A,T)
-      end
+    #sleep(0.1)
+    seekstart(io)
+    T = Array(TimeArray(Float64, io))
+    return(A,T)
+  end
 end
 
-#given the desired channel numbers, data directory, and data prefix, creates an analogdata object
-#recording num is to account for if there are multiple recordings, in which case the appropriate number is added to the filename
-function load_continuous_channels(prefix::String, data_directory::String, fs::Int64, channel_nums::Vector{Int64}, recording_num::Int64=1)
-x_all = Nullable{Array{Float64,2}}()
+#given the desired channel numbers, data directory, and data prefix,
+#creates an analogdata object
+#recording num is to account for if there are multiple recordings,
+#in which case the appropriate number is added to the filename
+function load_continuous_channels(prefix::String, data_directory::String,
+  fs::Int64, channel_nums::Vector{Int64}, recording_num::Int64=1)
+  x_all = Nullable{Array{Float64,2}}()
   t = nothing
   if recording_num==1
     recording_num_str = ""
@@ -54,33 +62,9 @@ x_all = Nullable{Array{Float64,2}}()
   return data
 end
 
-type Session
-  name::String
-  fs_openephys::Int64
-  directory::String
-  eeg_data::Nullable{AnalogData}
-  spectrum::Nullable{AnalogData}
-  ica::Nullable{AnalogData}
-  ica_spectrum::Nullable{AnalogData}
-end
-
-function Session(name::String, directory::String, eeg_data=Nullable{AnalogData}())
-  S = Session(name, 30000, directory, eeg_data, Nullable{AnalogData}(), Nullable{AnalogData}(), Nullable{AnalogData}())
-end
-
-function load_eeg(session::Session, channel_nums::Vector{Int64})
-  if !isnull(session.eeg_data)
-    error("load_eeg: eeg_data already loaded")
-  else
-  session.eeg_data = load_continuous_channels("100_CH", session.directory, session.fs_openephys, channel_nums)
-  end
-end
-
-#you can get the eeg_data in the form of a non-nullable analog data type by using get(session.eeg_data)
-
 type Spectrogram
   analog_data::AnalogData
-  power_all::Array{Float64,2}
+  power_all
   freq_bins::DSP.Util.Frequencies
   time_bins::FloatRange{Float64}
   time::Array{Float64,1}
@@ -88,15 +72,43 @@ end
 
 function Spectrogram(analog_data::AnalogData)
   power_all = Array{Float64}[]
-  freq_bins = DSP.Util.Frequencies
-  time_bins = FloatRange{Float64}
+  freq_bins = nothing
+  time_bins = nothing
   for chan_name = 1:(size(analog_data.channel_nums)[1])
-    s = spectrogram(analog_data.x_all[chan_name, :])
+    #TODO is 1024 the n we want? should n be an argument? (decides time bins)
+    s = spectrogram(analog_data.x_all[chan_name, :], 1024)
     freq_bins = freq(s)
     time_bins = time(s)
-    power_x = power(s) #should this be 1209601 by 15?
+    #TODO think about clamp range below!
+    power_x = 20.*log10(clamp!(power(s), .00001 , 100000000))
     push!(power_all, power_x)
   end
   ti = analog_data.t #is this the same as converting time bins back to time?
   S = Spectrogram(analog_data, power_all, freq_bins, time_bins, ti)
 end
+
+type Session
+  name::String
+  fs_openephys::Int64
+  directory::String
+  eeg_data::Nullable{AnalogData}
+  spectrum::Nullable{Spectrogram}
+  ica::Nullable{AnalogData}
+  ica_spectrum::Nullable{Spectrogram}
+end
+
+function Session(name::String, directory::String, eeg_data=Nullable{AnalogData}())
+  S = Session(name, 30000, directory, eeg_data, Nullable{Spectrogram}(),
+  Nullable{AnalogData}(), Nullable{Spectrogram}())
+end
+
+function load_eeg(session::Session, channel_nums::Vector{Int64})
+  if !isnull(session.eeg_data)
+    error("load_eeg: eeg_data already loaded")
+  else
+    session.eeg_data = load_continuous_channels("100_CH", session.directory,
+    session.fs_openephys, channel_nums)
+  end
+end
+
+#you can get the eeg_data in the form of a non-nullable analog data type by using get(session.eeg_data)
